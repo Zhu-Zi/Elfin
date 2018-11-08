3d64b2447d7fe499c107ee0e34e9fddd8a5f9a67
Zhu-Zi <513444173@qq.com>
2018-11-08 21:42:59 +0800

    添加文件输入输出辅助类

---
+++ Elfin/Elfin.Toolkits/Tools/FileIOHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Elfin.Toolkits.Tools
+{
+    /// <summary>
+    /// 文件输入输出辅助类
+    /// </summary>
+    public class FileIOHelper
+    {
+        private static List<string> _allSubdirectoryPathList = new List<string>();
+
+        /// <summary>
+        /// 获取指定路径下的全部文件
+        /// </summary>
+        /// <param name="path">路径</param>
+        /// <param name="searchPattern">搜索模式（Eg:"*.csv"）</param>
+        /// <returns></returns>
+        public static List<string> GetAllFilesByPath(string path, string searchPattern = "*")
+        {
+            var result = new List<string>();
+            var data = Directory.GetFiles(path, searchPattern);
+
+            if (data.Length > 0)
+            {
+                result = data.ToList();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 通过路径递归获取该路径下的所有子目录
+        /// </summary>
+        /// <param name="rootPath">路径</param>
+        /// <returns>路径下的全部子目录合集</returns>
+        public static List<string> GetAllSubdirectoryByPath(string rootPath)
+        {
+            var data = Directory.GetDirectories(rootPath);
+
+            foreach (var item in data)
+            {
+                _allSubdirectoryPathList.Add(item);
+            }
+
+            foreach (var item in data)
+            {
+                GetAllSubdirectoryByPath(item);
+            }
+
+            return _allSubdirectoryPathList;
+        }
+    }
+}
